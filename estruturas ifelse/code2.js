e10eefea69edda9bcd405e4643924efee29848ba
JoaoGabriel-JG <joao.nitsus@gmail.com>
2021-12-22 20:39:13 -0300

    Create code2.js

---
+++ estruturas ifelse/code2.js
@@ -0,0 +1,21 @@
+let numero = 1;
+
+//1 INICIAR
+//2 PAUSAR
+//3 AVANÇAR
+//4 RETROCEDER
+//5 DESLIGAR
+
+if(numero == 1)
+    console.log("INICIAR")
+else if(numero == 2)
+    console.log("PAUSAR")
+else if(numero == 3)
+    console.log("AVANÇAR")
+else if(numero == 4)
+    console.log("RETROCEDER")
+else if(numero == 5)
+    console.log("DESLIGAR")
+else 
+    console.log("Comando invalido")
+
